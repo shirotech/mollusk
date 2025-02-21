1e28d5473bd7c740b380ae16105d1e3eed835d8d
Joe C <joe.caulfield@anza.xyz>
2025-02-21 11:14:25 -0700

    Bump Solana and Nightly in CI/scripts (#90)

--- scripts/prepublish.sh
+++ scripts/prepublish.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-agave-install init 2.1.0
+agave-install init 2.2.0
 rm -rf target
 cargo build
 ./scripts/build-test-programs.sh
-cargo +nightly-2024-08-08 fmt --all -- --check
-cargo +nightly-2024-08-08 clippy --all --all-features -- -D warnings
+cargo +nightly-2024-11-22 fmt --all -- --check
+cargo +nightly-2024-11-22 clippy --all --all-features -- -D warnings
 cargo test --all-features
